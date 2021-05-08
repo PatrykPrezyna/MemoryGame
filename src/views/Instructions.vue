987e618e5bca5d8aeb2307b3d7e79fdb2eedfaea
PatrykPrezyna <81018358+PatrykPrezyna@users.noreply.github.com>
2021-05-08 17:35:04 +0200

    Initial commit

---
+++ src/views/Instructions.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <main class="main-instruction">
+      <h2>Instructions</h2>
+    </main>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Instructions",
+  data() {
+    return {};
+  }
+};
+</script>
