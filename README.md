3bea39006cdb7d0d5ff277b74aed671c40793dac
Patryk <patryk.prezyna@protonmail.com>
2022-07-10 22:02:52 +0200

    Reuse some ready project

--- README.md
+++ README.md
@@ -1,27 +1,28 @@
-# memory-game
-This is a simple project which allow me to master the basics of Vue.js.
-The idea to make online memory game with your own pictures was board when 
-I played with my family an offline version. It is a great way to socialise, 
-recall memories and train your brain at once. 
+# memory
 
 ## Project setup
 ```
-npm install
+yarn install
 ```
 
 ### Compiles and hot-reloads for development
 ```
-npm run serve
+yarn run serve
 ```
 
 ### Compiles and minifies for production
 ```
-npm run build
+yarn run build
+```
+
+### Run your tests
+```
+yarn run test
 ```
 
 ### Lints and fixes files
 ```
-npm run lint
+yarn run lint
 ```
 
 ### Customize configuration
--- src/App.vue
+++ src/App.vue
@@ -1,72 +1,324 @@
 <template>
-  <div id="app">
-    <header>
-      <h1 class="title">Matching Game</h1>
-    </header>
-    <div id="nav" ref="nav">
-      <router-link
-        to="/"
-        aria-current="page"
-        :active-class="$route.path == '/' ? 'router-link-active' : ''"
-        >Home</router-link
-      >|
-      <router-link to="/Instructions">Instructions</router-link>
+  <div id="app" class="app">
+    <div class="toolbar">
+
+
+
+        <ScoreBoard
+      :score="score"
+      @reset="reset"
+    />
     </div>
-    <router-view />
+      <button @click="reset" class="btn">
+        Reset game
+      </button>
+    <div
+      class="grid"
+    >
+      <Card
+        v-for="card in theCards"
+        :key="card.id"
+        :card="card"
+        :cardTransform="cardTransform"
+        @tapped="cardTapped"
+      />
+    </div>
+
   </div>
 </template>
 
-  <script>
+<script>
+import Card from './components/Card.vue';
+import ScoreBoard from './components/ScoreBoard.vue';
+
+// const animals = ['elephant', 'lion', 'fox', 'tiger', 'rabbit', 'owl'];
+
+/*const shapes = [
+  'triangle',
+  'star',
+  'rhombus',
+  'square',
+  'pentagon',
+  'hexagon',
+  'oval',
+  'circle'];
+*/
+const shapes = [
+  '1',
+  '2',
+  '3',
+  '4',
+  '5',
+  '6',
+  '7',
+  '8',
+  '9',
+  '10',
+  '11',
+  '12',
+  '13',
+  '14',
+  '15',
+  '16'];/*,
+  '17',
+  '18',
+  '19',
+  '20',
+  '21',
+  '22',
+  '23',
+  '24',
+  '25',
+  '26',
+  '27',
+  '28',
+  '29',
+  '30',
+  '31',
+  '32'];*/
+
+
+const Folder = 'Home'; /*'Rommelag';*/
+
+const cards = [];
+
+shapes.forEach(item => {
+
+
+  const card = {
+    matchKey: item,
+    flipped: false,
+    id: `${item}-a`,
+    imgUrl: `/images/shapes/${Folder}/${item}.jpg`,
+    matched: false,
+    order: 1,
+  };
+
+  // first copy
+  const cardA = card;
+  cards.push(cardA);
+  // second copy
+  const cardB = { ...card };
+  cardB.id = `${item}-b`;
+  cards.push(cardB);
+
+});
+
+function shuffle(a) {
+  for (let i = a.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [a[i], a[j]] = [a[j], a[i]];
+    a[i].order = i+1;
+  }
+  a[0].order = 1;
+  return a;
+}
+
+
+function initState() {
+
+  shuffle(cards);
+
+  return {
+    totalFlips: 0,
+    // theCards: this.shuffle(cards),
+    theCards: cards,
+    flipsThisTurn: 0,
+    turnCount: 0,
+    firstFlipID: null,
+    firstFlipMatchKey: null,
+    score: [],
+    cardTransform: null,
+  };
+}
+
 export default {
-  name: "home",
+  name: 'App',
+  components: {
+    Card,
+    ScoreBoard,
+  },
+
+  data() {
+    return initState();
+  },
+
+  computed: {
+    matchCount() {
+      return this.theCards.filter(card => card.matched === true).length / 2;
+    },
+  },
+
+  mounted() {
+    if (navigator.serviceWorker && !navigator.serviceWorker.controller) { navigator.serviceWorker.register('/serviceworker.js'); }
+  },
+
+  created() {
+    // attempt to use acceleromater for iOS less than 13
+    window.addEventListener('deviceorientation', this.handleOrientation);
+  },
+
+  methods: {
+    askMotionPermission() {
+      window.DeviceMotionEvent
+        .requestPermission()
+        .then(response => {
+          if (response === 'granted') {
+            window.addEventListener('deviceorientation', this.handleOrientation);
+          } else {
+            alert('OK, but you’re missing out...');
+          }
+        });
+    },
+
+    handleOrientation(event) {
+      this.cardTransform =  `transform: rotateZ(${event.alpha}deg) rotateY(${event.beta}deg) rotateX(${event.gamma}deg)`;
+    },
+
+    incrementFlipsThisTurn() {
+      this.flipsThisTurn ++;
+    },
+
+    cardTapped(tappedCardID) {
+      // store a copy of the card data as tappedCard
+      const tappedCard = this.theCards.find(obj => obj.id === tappedCardID);
+
+      if (tappedCard.flipped) {
+        return;
+      }
+
+      // Increment flip count for this turn
+      this.incrementFlipsThisTurn();
+
+      if (this.flipsThisTurn === 1) {
+        this.runTurn1(tappedCard);
+
+      }
+      if (this.flipsThisTurn === 2) {
+        this.runTurn2(tappedCard);
+      }
+    },
+
+    runTurn1(tappedCard) {
+      // flip the card face up
+      this.flipCard(tappedCard.id);
+
+      // store ID and Matchkey of first flipped card
+      this.firstFlipID = tappedCard.id;
+      this.firstFlipMatchKey = tappedCard.matchKey;
+    },
+
+    runTurn2(tappedCard) {
+      // flip the card face up
+      this.flipCard(tappedCard.id);
+      this.checkMatch(tappedCard);
+    },
+
+    checkMatch(tappedCard) {
+      // check match
+      setTimeout(() => {
+        if (tappedCard.matchKey === this.firstFlipMatchKey) {
+          // Match!
+          // reset flips counter
+          this.flipsThisTurn = 0;
+          // update the 2 cards 'matched' prop
+          const newCards = this.theCards.map(card => ([tappedCard.id, this.firstFlipID].includes(card.id)) ? { ...card, matched: true } : card );
+          this.theCards = newCards;
+
+          // update score
+          this.score.push('match');
+
+        } else {
+          // Not a match
+          // Turn both cards back face down
+          this.flipCard(tappedCard.id);
+          this.flipCard(this.firstFlipID);
+          this.flipsThisTurn = 0;
+          // update the score
+          this.score.push('miss');
+        }
+        // increment turn count
+        this.turnCount ++;
+
+      }, 2000);
+    },
+
+
+    flipCard(tappedCardID) {
+      const newCards = this.theCards.map(card => card.id === tappedCardID ? { ...card, flipped: !card.flipped } : card );
+      // update cards
+      this.theCards = newCards;
+    },
+    reset() {
+      Object.assign(this.$data, initState());
+    },
+  },
 };
+
 </script>
 
 <style lang="scss">
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-.title {
-  margin: 2em auto 0.5em;
+
+body {
+  background: #222222; //$c-gray-dark;
+  color: red; //$c-red;
+  padding: 0;
+  margin: 0;
+  font-family: arial, helvetica, sans-serif;
+  font-size: 15px;
 }
 
-#nav {
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    // text-decoration: none;
+.toolbar {
+  position: fixed;
+  right: 12px;
+  top: 15px;
+  z-index: 3;
+}
 
-    &.router-link-active {
-      text-decoration: none;
-      color: #0b5891;
-      border-bottom: 2px solid #0b5891;
-    }
+.btn {
+  appearance: none;
+  background: transparent;
+  border: 2px solid white;
+  color: white;
+  font: 11px / 1 arial;
+  letter-spacing: 0.3px;
+  padding: 4px 12px;
+  height: 24px;
+  border-radius: 12px;
+  margin-bottom: 10px;
+}
+
+.btn:active {
+  color: black;
+  background-color: white;
+}
+
+
+
+.app {
+  margin: 0;
+  padding: 20px;
+
+  @media (min-width: 700px) {
+    padding: 50px;
   }
 }
 
-html {
-  box-sizing: border-box;
-}
-
-*,
-*::before,
-*::after {
-  box-sizing: inherit;
-}
-
-html,
-body {
-  width: 100%;
-  height: 100%;
+.grid {
+  list-style: none;
   margin: 0;
   padding: 0;
+  display: grid;
+  grid-gap: 20px;
+  grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr; // 1fr 1fr; // 1fr 1fr; // 1fr 1fr;
+  width: calc(100vw - 80px);
+  height: calc(100vh - 100px);
 }
 
-body {
-  background: #ffffff url("imgs/fabric.png");
+.grid__cell {
+  // width: 100%;
+  // height: 100%;
 }
+
+
 </style>
